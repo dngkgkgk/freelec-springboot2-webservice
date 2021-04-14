d6596cfcec17a343a8d1e0240742bd49952185ee
wnstjrdia <wnstjrdia@naver.com>
2021-04-14 10:30:51 +0900

    deploy수정66

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,7 +9,7 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인해봅시다!!"
 
-CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep java | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl ec2-user | grep java | awk '{print $1}')
 
 echo "현재 구동중인 어플리케이션은요?????id: $CURRENT_PID"
 
