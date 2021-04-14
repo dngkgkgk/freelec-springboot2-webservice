a0e0a2ec7d25ed5f71b1674905814cbada85214e
wnstjrdia <wnstjrdia@naver.com>
2021-04-14 09:37:48 +0900

    deploy수정33

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@ echo "> Build 파일 복사"
 
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
-echo "> 현재 구동중인 애플리케이션 pid 확인해봅시다"
+echo "> 현재 구동중인 애플리케이션 pid 확인해봅시다!!"
 
 CURRENT_PID=$(pgrep -fl freelect-springboot2-webservice | grep java | awk '{print $1}')
 
