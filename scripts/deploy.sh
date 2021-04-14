6fbf8de2b317d3ea35d494feee7aef1001e739a1
wnstjrdia <wnstjrdia@naver.com>
2021-04-14 09:43:33 +0900

    deploy수정44

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 REPOSITORY=/home/ec2-user/app/step2
-PROJECT_NAME=freelect-springboot2-webservice
+PROJECT_NAME=freelec-springboot2-webservice
 
 echo "> Build 파일 복사"
 
@@ -9,9 +9,9 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인해봅시다!!"
 
-CURRENT_PID=$(pgrep -fl freelect-springboot2-webservice | grep java | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl freelec-springboot2-webservice | grep java | awk '{print $1}')
 
-echo "현재 구동중인 어플리케이션은요??id: $CURRENT_PID"
+echo "현재 구동중인 어플리케이션은요?????id: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
     echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
