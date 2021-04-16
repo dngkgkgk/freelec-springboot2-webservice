165d3d622061ea331610d7c419f654b94c2eaee5
wnstjrdia <wnstjrdia@naver.com>
2021-04-16 14:41:13 +0900

    sh파일 푸쉬22

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-ABSPATH = $(readlink -f $0)
-ABSDIR = $(dirname $ABSPATH)
-source = ${ABSDIR}/profile.sh
+ABSPATH=$(readlink -f $0)
+ABSDIR=$(dirname $ABSPATH)
+source ${ABSDIR}/profile.sh
 
 REPOSITORY=/home/ec2-user/app/step3
 PROJECT_NAME=book
